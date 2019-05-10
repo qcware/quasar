4ee97ae82c631e87b7edaacde874992da6e8f18a
Rob Parrish <robparrish@gmail.com>
2019-05-09 23:57:44 -0400

    Kill Q fields for now

--- quasar/aiem/aiem_data.py
+++ quasar/aiem/aiem_data.py
@@ -78,9 +78,6 @@ class AIEMMonomer(AIEMConnectivity):
         EH,
         ET,
         EP,
-        QH,
-        QT,
-        QP,
         MH,
         MT,
         MP,
@@ -92,9 +89,6 @@ class AIEMMonomer(AIEMConnectivity):
         self.EH = EH
         self.ET = ET
         self.EP = EP
-        self.QH = QH
-        self.QT = QT
-        self.QP = QP
         self.MH = MH
         self.MT = MT
         self.MP = MP
@@ -103,9 +97,6 @@ class AIEMMonomer(AIEMConnectivity):
         if not isinstance(self.EH, np.ndarray): raise RuntimeError('EH must be np.ndarray')    
         if not isinstance(self.ET, np.ndarray): raise RuntimeError('ET must be np.ndarray')    
         if not isinstance(self.EP, np.ndarray): raise RuntimeError('EP must be np.ndarray')    
-        if not isinstance(self.QH, np.ndarray): raise RuntimeError('QH must be np.ndarray')    
-        if not isinstance(self.QT, np.ndarray): raise RuntimeError('QT must be np.ndarray')    
-        if not isinstance(self.QP, np.ndarray): raise RuntimeError('QP must be np.ndarray')    
         if not isinstance(self.MH, np.ndarray): raise RuntimeError('MH must be np.ndarray')    
         if not isinstance(self.MT, np.ndarray): raise RuntimeError('MT must be np.ndarray')    
         if not isinstance(self.MP, np.ndarray): raise RuntimeError('MP must be np.ndarray')    
@@ -114,9 +105,6 @@ class AIEMMonomer(AIEMConnectivity):
         if self.EH.shape != (self.N,): raise RuntimeError('EH.shape != (N,)')
         if self.ET.shape != (self.N,): raise RuntimeError('ET.shape != (N,)')
         if self.EP.shape != (self.N,): raise RuntimeError('EP.shape != (N,)')
-        if self.QH.shape != (self.N,): raise RuntimeError('QH.shape != (N,)')
-        if self.QT.shape != (self.N,): raise RuntimeError('QT.shape != (N,)')
-        if self.QP.shape != (self.N,): raise RuntimeError('QP.shape != (N,)')
         if self.MH.shape != (self.N,3): raise RuntimeError('MH.shape != (N,3)')
         if self.MT.shape != (self.N,3): raise RuntimeError('MT.shape != (N,3)')
         if self.MP.shape != (self.N,3): raise RuntimeError('MP.shape != (N,3)')
@@ -125,9 +113,6 @@ class AIEMMonomer(AIEMConnectivity):
         if self.EH.dtype != np.float64: raise RuntimeError('EH.dtype must be np.float64')
         if self.ET.dtype != np.float64: raise RuntimeError('ET.dtype must be np.float64')
         if self.EP.dtype != np.float64: raise RuntimeError('EP.dtype must be np.float64')
-        if self.QH.dtype != np.float64: raise RuntimeError('QH.dtype must be np.float64')
-        if self.QT.dtype != np.float64: raise RuntimeError('QT.dtype must be np.float64')
-        if self.QP.dtype != np.float64: raise RuntimeError('QP.dtype must be np.float64')
         if self.MH.dtype != np.float64: raise RuntimeError('MH.dtype must be np.float64')
         if self.MT.dtype != np.float64: raise RuntimeError('MT.dtype must be np.float64')
         if self.MP.dtype != np.float64: raise RuntimeError('MP.dtype must be np.float64')
@@ -140,9 +125,6 @@ class AIEMMonomer(AIEMConnectivity):
             EH=np.zeros_like(other.EH), 
             ET=np.zeros_like(other.ET), 
             EP=np.zeros_like(other.EP), 
-            QH=np.zeros_like(other.QH), 
-            QT=np.zeros_like(other.QT), 
-            QP=np.zeros_like(other.QP), 
             MH=np.zeros_like(other.MH), 
             MT=np.zeros_like(other.MT), 
             MP=np.zeros_like(other.MP), 
@@ -156,9 +138,6 @@ class AIEMMonomer(AIEMConnectivity):
             EH=np.copy(other.EH), 
             ET=np.copy(other.ET), 
             EP=np.copy(other.EP), 
-            QH=np.copy(other.QH), 
-            QT=np.copy(other.QT), 
-            QP=np.copy(other.QP), 
             MH=np.copy(other.MH), 
             MT=np.copy(other.MT), 
             MP=np.copy(other.MP), 
@@ -217,9 +196,6 @@ class AIEMMonomer(AIEMConnectivity):
             EH=dat['EH'][:N],
             ET=dat['ET'][:N],
             EP=dat['EP'][:N],
-            QH=np.zeros((N,)),
-            QT=np.zeros((N,)),
-            QP=np.zeros((N,)),
             MH=dat['MH'][:N,:],
             MT=dat['MT'][:N,:],
             MP=dat['MP'][:N,:],
@@ -235,7 +211,6 @@ class AIEMMonomer(AIEMConnectivity):
     @staticmethod
     def from_tc_exciton_files(
         filenames,
-        charges=None,
         N=None,
         connectivity='all',
         zero_gauge=False,
@@ -247,11 +222,6 @@ class AIEMMonomer(AIEMConnectivity):
         if N > len(filenames):
             raise RuntimeError('N > len(filenames)')
     
-        if charges: raise NotImplemented
-        
-        if charges is None:
-            charges = np.zeros((N,))
-
         # Connectivity Matrix
         connectivity2 = np.zeros((N,N), dtype=np.bool)
         for A in range(N):
@@ -272,17 +242,10 @@ class AIEMMonomer(AIEMConnectivity):
         EH = np.zeros((N,))
         ET = np.zeros((N,))
         EP = np.zeros((N,))
-        QH = np.zeros((N,))
-        QT = np.zeros((N,))
-        QP = np.zeros((N,))
         MH = np.zeros((N,3))
         MT = np.zeros((N,3))
         MP = np.zeros((N,3))
         R0 = np.zeros((N,3))
-
-        # Fixed H/P charges
-        QH = np.array(charges[:N])
-        QP = np.array(charges[:N])
 
         re_EH = re.compile(r'^Energy\s+0:\s+(\S+)\s*$')
         re_EP = re.compile(r'^Energy\s+1:\s+(\S+)\s*$')
@@ -333,9 +296,6 @@ class AIEMMonomer(AIEMConnectivity):
             EH=EH,
             ET=ET,
             EP=EP,
-            QH=QH,
-            QT=QT,
-            QP=QP,
             MH=MH,
             MT=MT,
             MP=MP,
@@ -358,9 +318,6 @@ class AIEMMonomerGrad(AIEMConnectivity):
         EH,
         ET,
         EP,
-        QH,
-        QT,
-        QP,
         MH,
         MT,
         MP,
@@ -372,9 +329,6 @@ class AIEMMonomerGrad(AIEMConnectivity):
         self.EH = EH
         self.ET = ET
         self.EP = EP
-        self.QH = QH
-        self.QT = QT
-        self.QP = QP
         self.MH = MH
         self.MT = MT
         self.MP = MP
@@ -383,9 +337,6 @@ class AIEMMonomerGrad(AIEMConnectivity):
         if not isinstance(self.EH, list): raise RuntimeError('EH must be list')    
         if not isinstance(self.ET, list): raise RuntimeError('ET must be list')    
         if not isinstance(self.EP, list): raise RuntimeError('EP must be list')    
-        if not isinstance(self.QH, list): raise RuntimeError('QH must be list')    
-        if not isinstance(self.QT, list): raise RuntimeError('QT must be list')    
-        if not isinstance(self.QP, list): raise RuntimeError('QP must be list')    
         if not isinstance(self.MH, list): raise RuntimeError('MH must be list')    
         if not isinstance(self.MT, list): raise RuntimeError('MT must be list')    
         if not isinstance(self.MP, list): raise RuntimeError('MP must be list')    
@@ -394,9 +345,6 @@ class AIEMMonomerGrad(AIEMConnectivity):
         if len(self.EH) != self.N: raise RuntimeError('len(EH) != N')
         if len(self.ET) != self.N: raise RuntimeError('len(ET) != N')
         if len(self.EP) != self.N: raise RuntimeError('len(EP) != N')
-        if len(self.QH) != self.N: raise RuntimeError('len(QH) != N')
-        if len(self.QT) != self.N: raise RuntimeError('len(QT) != N')
-        if len(self.QP) != self.N: raise RuntimeError('len(QP) != N')
         if len(self.MH) != self.N: raise RuntimeError('len(MH) != N')
         if len(self.MT) != self.N: raise RuntimeError('len(MT) != N')
         if len(self.MP) != self.N: raise RuntimeError('len(MP) != N')
@@ -407,9 +355,6 @@ class AIEMMonomerGrad(AIEMConnectivity):
             if not isinstance(self.EH[I], np.ndarray): raise RuntimeError('EH[I] must be np.ndarray')    
             if not isinstance(self.ET[I], np.ndarray): raise RuntimeError('ET[I] must be np.ndarray')    
             if not isinstance(self.EP[I], np.ndarray): raise RuntimeError('EP[I] must be np.ndarray')    
-            if not isinstance(self.QH[I], np.ndarray): raise RuntimeError('QH[I] must be np.ndarray')    
-            if not isinstance(self.QT[I], np.ndarray): raise RuntimeError('QT[I] must be np.ndarray')    
-            if not isinstance(self.QP[I], np.ndarray): raise RuntimeError('QP[I] must be np.ndarray')    
             if not isinstance(self.MH[I], np.ndarray): raise RuntimeError('MH[I] must be np.ndarray')    
             if not isinstance(self.MT[I], np.ndarray): raise RuntimeError('MT[I] must be np.ndarray')    
             if not isinstance(self.MP[I], np.ndarray): raise RuntimeError('MP[I] must be np.ndarray')    
@@ -418,9 +363,6 @@ class AIEMMonomerGrad(AIEMConnectivity):
             if self.EH[I].shape != (self.natom[I],3): raise RuntimeError('EH.shape != (natom[I],3)')
             if self.ET[I].shape != (self.natom[I],3): raise RuntimeError('ET.shape != (natom[I],3)')
             if self.EP[I].shape != (self.natom[I],3): raise RuntimeError('EP.shape != (natom[I],3)')
-            if self.QH[I].shape != (self.natom[I],3): raise RuntimeError('QH.shape != (natom[I],3)')
-            if self.QT[I].shape != (self.natom[I],3): raise RuntimeError('QT.shape != (natom[I],3)')
-            if self.QP[I].shape != (self.natom[I],3): raise RuntimeError('QP.shape != (natom[I],3)')
             if self.MH[I].shape != (3,self.natom[I],3): raise RuntimeError('MH.shape != (3,natom[I],3)')
             if self.MT[I].shape != (3,self.natom[I],3): raise RuntimeError('MT.shape != (3,natom[I],3)')
             if self.MP[I].shape != (3,self.natom[I],3): raise RuntimeError('MP.shape != (3,natom[I],3)')
@@ -429,9 +371,6 @@ class AIEMMonomerGrad(AIEMConnectivity):
             if self.EH[I].dtype != np.float64: raise RuntimeError('EH[I].dtype must be np.float64')
             if self.ET[I].dtype != np.float64: raise RuntimeError('ET[I].dtype must be np.float64')
             if self.EP[I].dtype != np.float64: raise RuntimeError('EP[I].dtype must be np.float64')
-            if self.QH[I].dtype != np.float64: raise RuntimeError('QH[I].dtype must be np.float64')
-            if self.QT[I].dtype != np.float64: raise RuntimeError('QT[I].dtype must be np.float64')
-            if self.QP[I].dtype != np.float64: raise RuntimeError('QP[I].dtype must be np.float64')
             if self.MH[I].dtype != np.float64: raise RuntimeError('MH[I].dtype must be np.float64')
             if self.MT[I].dtype != np.float64: raise RuntimeError('MT[I].dtype must be np.float64')
             if self.MP[I].dtype != np.float64: raise RuntimeError('MP[I].dtype must be np.float64')
@@ -444,9 +383,6 @@ class AIEMMonomerGrad(AIEMConnectivity):
             EH=[np.zeros_like(_) for _ in other.EH],
             ET=[np.zeros_like(_) for _ in other.ET],
             EP=[np.zeros_like(_) for _ in other.EP],
-            QH=[np.zeros_like(_) for _ in other.QH],
-            QT=[np.zeros_like(_) for _ in other.QT],
-            QP=[np.zeros_like(_) for _ in other.QP],
             MH=[np.zeros_like(_) for _ in other.MH],
             MT=[np.zeros_like(_) for _ in other.MT],
             MP=[np.zeros_like(_) for _ in other.MP],
@@ -460,9 +396,6 @@ class AIEMMonomerGrad(AIEMConnectivity):
             EH=[np.copy(_) for _ in other.EH],
             ET=[np.copy(_) for _ in other.ET],
             EP=[np.copy(_) for _ in other.EP],
-            QH=[np.copy(_) for _ in other.QH],
-            QT=[np.copy(_) for _ in other.QT],
-            QP=[np.copy(_) for _ in other.QP],
             MH=[np.copy(_) for _ in other.MH],
             MT=[np.copy(_) for _ in other.MT],
             MP=[np.copy(_) for _ in other.MP],
@@ -535,7 +468,6 @@ class AIEMMonomerGrad(AIEMConnectivity):
     @staticmethod
     def from_tc_exciton_files(
         filenames,
-        charges=None,
         N=None,
         connectivity='all',
         zero_gauge=False,
@@ -547,9 +479,6 @@ class AIEMMonomerGrad(AIEMConnectivity):
         if N > len(filenames):
             raise RuntimeError('N > len(filenames)')
     
-        if charges is None:
-            charges = np.zeros((N,))
-
         # Connectivity Matrix
         connectivity2 = np.zeros((N,N), dtype=np.bool)
         for A in range(N):
@@ -659,18 +588,12 @@ class AIEMMonomerGrad(AIEMConnectivity):
         # Definitionally zero
         natom = [_.shape[0] for _ in EH]
         ET = [np.zeros((_,3)) for _ in natom]
-        QH = [np.zeros((_,3)) for _ in natom]
-        QT = [np.zeros((_,3)) for _ in natom]
-        QP = [np.zeros((_,3)) for _ in natom]
 
         monomer = AIEMMonomerGrad(
             connectivity=connectivity2,
             EH=EH,
             ET=ET,
             EP=EP,
-            QH=QH,
-            QT=QT,
-            QP=QP,
             MH=MH,
             MT=MT,
             MP=MP,
@@ -960,15 +883,15 @@ class AIEMUtil(object):
         VPP = np.zeros((monomer.N,)*2)
 
         tasks = [
-            (monomer.QH, monomer.QH, monomer.MH, monomer.MH, VHH),
-            (monomer.QH, monomer.QT, monomer.MH, monomer.MT, VHT),
-            (monomer.QH, monomer.QP, monomer.MH, monomer.MP, VHP),
-            (monomer.QT, monomer.QH, monomer.MT, monomer.MH, VTH),
-            (monomer.QT, monomer.QT, monomer.MT, monomer.MT, VTT),
-            (monomer.QT, monomer.QP, monomer.MT, monomer.MP, VTP),
-            (monomer.QP, monomer.QH, monomer.MP, monomer.MH, VPH),
-            (monomer.QP, monomer.QT, monomer.MP, monomer.MT, VPT),
-            (monomer.QP, monomer.QP, monomer.MP, monomer.MP, VPP),
+            (monomer.MH, monomer.MH, VHH),
+            (monomer.MH, monomer.MT, VHT),
+            (monomer.MH, monomer.MP, VHP),
+            (monomer.MT, monomer.MH, VTH),
+            (monomer.MT, monomer.MT, VTT),
+            (monomer.MT, monomer.MP, VTP),
+            (monomer.MP, monomer.MH, VPH),
+            (monomer.MP, monomer.MT, VPT),
+            (monomer.MP, monomer.MP, VPP),
             ]
 
         for A, B in monomer.ABs:
@@ -976,16 +899,10 @@ class AIEMUtil(object):
             R0B = monomer.R0[B,:]
             RAB = R0B - R0A
             DAB = np.sqrt(np.sum(RAB**2))
-            for QAs, QBs, MAs, MBs, Vs in tasks:
-                QA = QAs[A]
-                QB = QBs[B]
+            for MAs, MBs, Vs in tasks:
                 MA = MAs[A,:]
                 MB = MBs[B,:]
                 V = 0.0
-                # Charge-Charge
-                V += 1.0 * QA * QB / DAB
-                # Charge-Dipole
-                V += 1.0 * QA * np.sum(MA * RAB) / DAB**3
                 # Dipole-Dipole
                 V += 1.0 * np.sum(MA * MB) / DAB**3
                 V -= 3.0 * np.sum(MA * RAB) * np.sum(MB * RAB) / DAB**5
