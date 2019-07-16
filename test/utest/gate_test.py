d1cd9a7460e25f595b2b267c82f028de5dbedd27
Farquhar13 <farquhar13@gmail.com>
2019-07-16 15:06:28 -0700

    gate class test

--- test/utest/gate_test.py
+++ test/utest/gate_test.py
@@ -22,6 +22,28 @@ class Test(unittest.TestCase):
         self.assertTrue(gate_class_tests.set_params())
     def test_set_params1(self):
         self.assertTrue(gate_class_tests.set_params1())
+    def test_set_param(self):
+        self.assertTrue(gate_class_tests.set_param())
+    def test_set_param1(self):
+        self.assertTrue(gate_class_tests.set_param1())
+    def test_gateRx(self):
+        self.assertTrue(gate_class_tests.gateRx())
+    def test_gateRy(self):
+        self.assertTrue(gate_class_tests.gateRy())
+    def test_gateRz(self):
+        self.assertTrue(gate_class_tests.gateRz())
+    def test_gateSO4(self):
+        self.assertTrue(gate_class_tests.gateSO4())
+    def test_gateSO42(self):
+        self.assertTrue(gate_class_tests.gateSO42())
+    def test_gateCF(self):
+        self.assertTrue(gate_class_tests.gateCF())
+    def test_gateU1(self):
+        self.assertTrue(gate_class_tests.gateU1())
+    def test_gateU2(self):
+        self.assertTrue(gate_class_tests.gateU2())
+    def test_init_controlled_gate(self):
+        self.assertTrue(gate_class_tests.init_controlled_gate())
 
 if __name__ == '__main__':
     unittest.main()
