282f8ee38f4dd76bfbb80d18096463e26b881ddf
Rob Parrish <robparrish@gmail.com>
2019-11-07 05:33:30 +0000

    Vulcan types

---
+++ vulcan-test/test2.py
@@ -0,0 +1,74 @@
+import quasar
+import vulcan
+import numpy as np
+import time
+import sys
+
+N = int(sys.argv[1]) # Increase this to get some more dramatic timing examples below
+
+gadget = quasar.Circuit().Ry(1).CZ(0,1).Ry(1).CX(1,0)
+print(gadget)    
+
+circuit = quasar.Circuit().X(0)
+circuit.T(0)
+for I in range(N):
+    circuit.add_gates(circuit=gadget, qubits=(I, I+1))
+for I in range(N+1):
+    circuit.T(I)
+# circuit.T(N)
+# circuit.TT(N)
+circuit = circuit.slice(qubits=list(reversed(range(N+1))))
+circuit.Ry(0, theta=0.3, time_placement='next')
+print(circuit)
+
+parameter_values = []
+for I in range(N):
+    value = (1.0 - I / 17.0)
+    parameter_values.append(+value)
+    parameter_values.append(-value)
+circuit.set_parameter_values(parameter_values)
+print(circuit.parameter_str)
+
+I, X, Y, Z = quasar.Pauli.IXYZ()
+pauli = quasar.Pauli.zero()
+for k in range(N+1):
+    pauli += (k + 1) / 10.0 * Z[k]
+print(pauli)
+
+# backend = quasar.QuasarSimulatorBackend()
+
+# start = time.time()
+# statevector1 = backend.run_statevector(circuit)
+# print('%11.3E' % (time.time() - start))
+
+start = time.time()
+statevector2 = vulcan.run_statevector(circuit)
+print('%11.3E' % (time.time() - start))
+
+# print(np.sum(statevector1.conj() * statevector2))
+
+sigma = vulcan.run_pauli_sigma(pauli, statevector2)
+print('Vulcan + CPU:')
+print(np.sum(sigma.conj() * statevector2))
+
+# print('CPU:')
+# start = time.time()
+# energy = backend.run_pauli_expectation_value(circuit, pauli)
+# print('%11.3E' % (time.time() - start))
+# print(energy)
+
+print('Vulcan:')
+start = time.time()
+energy = vulcan.run_pauli_expectation_value(circuit, pauli)
+print('%11.3E' % (time.time() - start))
+print(energy)
+
+# start = time.time()
+# gradient = backend.run_pauli_expectation_value_gradient(circuit, pauli)
+# print('%11.3E' % (time.time() - start))
+# print(gradient)
+
+start = time.time()
+gradient = vulcan.run_pauli_expectation_value_gradient(circuit, pauli)
+print('%11.3E' % (time.time() - start))
+print(gradient)
--- vulcan/vulcan_py.cpp
+++ vulcan/vulcan_py.cpp
@@ -31,6 +31,24 @@ py::array_t<U> py_run_statevector(
     return result;
 }
 
+np_float32 py_run_statevector_float32(
+    const Circuit<float32>& circuit)
+{
+    return py_run_statevector<float32, float>(circuit);
+}
+
+np_float64 py_run_statevector_float64(
+    const Circuit<float64>& circuit)
+{
+    return py_run_statevector<float64, double>(circuit);
+}
+
+np_complex64 py_run_statevector_complex64(
+    const Circuit<complex64>& circuit)
+{
+    return py_run_statevector<complex64, std::complex<float>>(circuit);
+}
+
 np_complex128 py_run_statevector_complex128(
     const Circuit<complex128>& circuit)
 {
@@ -56,6 +74,27 @@ py::array_t<U> py_run_pauli_sigma(
     return result;
 }
 
+np_float32 py_run_pauli_sigma_float32(
+    const Pauli<float32>& pauli,
+    const np_float32 statevector)
+{
+    return py_run_pauli_sigma<float32, float>(pauli, statevector);
+}
+
+np_float64 py_run_pauli_sigma_float64(
+    const Pauli<float64>& pauli,
+    const np_float64 statevector)
+{
+    return py_run_pauli_sigma<float64, double>(pauli, statevector);
+}
+
+np_complex64 py_run_pauli_sigma_complex64(
+    const Pauli<complex64>& pauli,
+    const np_complex64 statevector)
+{
+    return py_run_pauli_sigma<complex64, std::complex<float>>(pauli, statevector);
+}
+
 np_complex128 py_run_pauli_sigma_complex128(
     const Pauli<complex128>& pauli,
     const np_complex128 statevector)
@@ -76,6 +115,27 @@ U py_run_pauli_expectation_value(
     std::memcpy(&val2, &val, sizeof(T));
 
     return val2;
+}
+
+float py_run_pauli_expectation_value_float32(
+    const Circuit<float32>& circuit,
+    const Pauli<float32>& pauli)
+{
+    return py_run_pauli_expectation_value<float32, float>(circuit, pauli);
+}
+
+double py_run_pauli_expectation_value_float64(
+    const Circuit<float64>& circuit,
+    const Pauli<float64>& pauli)
+{
+    return py_run_pauli_expectation_value<float64, double>(circuit, pauli);
+}
+
+std::complex<float> py_run_pauli_expectation_value_complex64(
+    const Circuit<complex64>& circuit,
+    const Pauli<complex64>& pauli)
+{
+    return py_run_pauli_expectation_value<complex64, std::complex<float>>(circuit, pauli);
 }
 
 std::complex<double> py_run_pauli_expectation_value_complex128(
@@ -101,6 +161,27 @@ std::vector<U> py_run_pauli_expectation_value_gradient(
     return val2;
 }
 
+std::vector<float> py_run_pauli_expectation_value_gradient_float32(
+    const Circuit<float32>& circuit,
+    const Pauli<float32>& pauli)
+{
+    return py_run_pauli_expectation_value_gradient<float32, float>(circuit, pauli);
+}
+
+std::vector<double> py_run_pauli_expectation_value_gradient_float64(
+    const Circuit<float64>& circuit,
+    const Pauli<float64>& pauli)
+{
+    return py_run_pauli_expectation_value_gradient<float64, double>(circuit, pauli);
+}
+
+std::vector<std::complex<float>> py_run_pauli_expectation_value_gradient_complex64(
+    const Circuit<complex64>& circuit,
+    const Pauli<complex64>& pauli)
+{
+    return py_run_pauli_expectation_value_gradient<complex64, std::complex<float>>(circuit, pauli);
+}
+
 std::vector<std::complex<double>> py_run_pauli_expectation_value_gradient_complex128(
     const Circuit<complex128>& circuit,
     const Pauli<complex128>& pauli)
@@ -112,6 +193,125 @@ PYBIND11_MODULE(vulcan_plugin, m) {
 
     m.def("ndevice", &vulcan::ndevice);
     m.def("device_property_string", &vulcan::device_property_string);
+
+    // => float32 <= //
+
+    py::class_<vulcan::float32>(m, "float32")
+    .def(py::init<float, float>())
+    .def("real", &vulcan::float32::real)
+    .def("imag", &vulcan::float32::imag)
+    ;
+
+    py::class_<vulcan::Gate<vulcan::float32>>(m, "Gate_float32")
+    .def(py::init<int, const std::string&, const std::vector<vulcan::float32>&>())
+    .def("nqubit", &vulcan::Gate<vulcan::float32>::nqubit)
+    .def("name", &vulcan::Gate<vulcan::float32>::name)
+    .def("matrix", &vulcan::Gate<vulcan::float32>::matrix)
+    .def("adjoint", &vulcan::Gate<vulcan::float32>::adjoint)
+    ; 
+
+    py::class_<vulcan::Circuit<vulcan::float32>>(m, "Circuit_float32")
+    .def(py::init<int, const std::vector<vulcan::Gate<vulcan::float32>>&, const std::vector<std::vector<int>>&>())
+    .def("nqubit", &vulcan::Circuit<vulcan::float32>::nqubit)
+    .def("gates", &vulcan::Circuit<vulcan::float32>::gates)
+    .def("qubits", &vulcan::Circuit<vulcan::float32>::qubits)
+    .def("adjoint", &vulcan::Circuit<vulcan::float32>::adjoint)
+    .def("bit_reversal", &vulcan::Circuit<vulcan::float32>::bit_reversal)
+    ; 
+
+    py::class_<vulcan::Pauli<vulcan::float32>>(m, "Pauli_float32")
+    .def(py::init<int, const std::vector<std::vector<int>>&, const std::vector<std::vector<int>>&, const std::vector<vulcan::float32>&>())
+    .def("nqubit", &vulcan::Pauli<vulcan::float32>::nqubit)
+    .def("types", &vulcan::Pauli<vulcan::float32>::types)
+    .def("qubits", &vulcan::Pauli<vulcan::float32>::qubits)
+    .def("values", &vulcan::Pauli<vulcan::float32>::values)
+    .def("bit_reversal", &vulcan::Pauli<vulcan::float32>::bit_reversal)
+    ; 
+
+    m.def("run_statevector_float32", py_run_statevector_float32);
+    m.def("run_pauli_sigma_float32", py_run_pauli_sigma_float32);
+    m.def("run_pauli_expectation_value_float32", py_run_pauli_expectation_value_float32);
+    m.def("run_pauli_expectation_value_gradient_float32", py_run_pauli_expectation_value_gradient_float32);
+
+    // => float64 <= //
+
+    py::class_<vulcan::float64>(m, "float64")
+    .def(py::init<double, double>())
+    .def("real", &vulcan::float64::real)
+    .def("imag", &vulcan::float64::imag)
+    ;
+
+    py::class_<vulcan::Gate<vulcan::float64>>(m, "Gate_float64")
+    .def(py::init<int, const std::string&, const std::vector<vulcan::float64>&>())
+    .def("nqubit", &vulcan::Gate<vulcan::float64>::nqubit)
+    .def("name", &vulcan::Gate<vulcan::float64>::name)
+    .def("matrix", &vulcan::Gate<vulcan::float64>::matrix)
+    .def("adjoint", &vulcan::Gate<vulcan::float64>::adjoint)
+    ; 
+
+    py::class_<vulcan::Circuit<vulcan::float64>>(m, "Circuit_float64")
+    .def(py::init<int, const std::vector<vulcan::Gate<vulcan::float64>>&, const std::vector<std::vector<int>>&>())
+    .def("nqubit", &vulcan::Circuit<vulcan::float64>::nqubit)
+    .def("gates", &vulcan::Circuit<vulcan::float64>::gates)
+    .def("qubits", &vulcan::Circuit<vulcan::float64>::qubits)
+    .def("adjoint", &vulcan::Circuit<vulcan::float64>::adjoint)
+    .def("bit_reversal", &vulcan::Circuit<vulcan::float64>::bit_reversal)
+    ; 
+
+    py::class_<vulcan::Pauli<vulcan::float64>>(m, "Pauli_float64")
+    .def(py::init<int, const std::vector<std::vector<int>>&, const std::vector<std::vector<int>>&, const std::vector<vulcan::float64>&>())
+    .def("nqubit", &vulcan::Pauli<vulcan::float64>::nqubit)
+    .def("types", &vulcan::Pauli<vulcan::float64>::types)
+    .def("qubits", &vulcan::Pauli<vulcan::float64>::qubits)
+    .def("values", &vulcan::Pauli<vulcan::float64>::values)
+    .def("bit_reversal", &vulcan::Pauli<vulcan::float64>::bit_reversal)
+    ; 
+
+    m.def("run_statevector_float64", py_run_statevector_float64);
+    m.def("run_pauli_sigma_float64", py_run_pauli_sigma_float64);
+    m.def("run_pauli_expectation_value_float64", py_run_pauli_expectation_value_float64);
+    m.def("run_pauli_expectation_value_gradient_float64", py_run_pauli_expectation_value_gradient_float64);
+
+    // => complex64 <= //
+
+    py::class_<vulcan::complex64>(m, "complex64")
+    .def(py::init<float, float>())
+    .def("real", &vulcan::complex64::real)
+    .def("imag", &vulcan::complex64::imag)
+    ;
+
+    py::class_<vulcan::Gate<vulcan::complex64>>(m, "Gate_complex64")
+    .def(py::init<int, const std::string&, const std::vector<vulcan::complex64>&>())
+    .def("nqubit", &vulcan::Gate<vulcan::complex64>::nqubit)
+    .def("name", &vulcan::Gate<vulcan::complex64>::name)
+    .def("matrix", &vulcan::Gate<vulcan::complex64>::matrix)
+    .def("adjoint", &vulcan::Gate<vulcan::complex64>::adjoint)
+    ; 
+
+    py::class_<vulcan::Circuit<vulcan::complex64>>(m, "Circuit_complex64")
+    .def(py::init<int, const std::vector<vulcan::Gate<vulcan::complex64>>&, const std::vector<std::vector<int>>&>())
+    .def("nqubit", &vulcan::Circuit<vulcan::complex64>::nqubit)
+    .def("gates", &vulcan::Circuit<vulcan::complex64>::gates)
+    .def("qubits", &vulcan::Circuit<vulcan::complex64>::qubits)
+    .def("adjoint", &vulcan::Circuit<vulcan::complex64>::adjoint)
+    .def("bit_reversal", &vulcan::Circuit<vulcan::complex64>::bit_reversal)
+    ; 
+
+    py::class_<vulcan::Pauli<vulcan::complex64>>(m, "Pauli_complex64")
+    .def(py::init<int, const std::vector<std::vector<int>>&, const std::vector<std::vector<int>>&, const std::vector<vulcan::complex64>&>())
+    .def("nqubit", &vulcan::Pauli<vulcan::complex64>::nqubit)
+    .def("types", &vulcan::Pauli<vulcan::complex64>::types)
+    .def("qubits", &vulcan::Pauli<vulcan::complex64>::qubits)
+    .def("values", &vulcan::Pauli<vulcan::complex64>::values)
+    .def("bit_reversal", &vulcan::Pauli<vulcan::complex64>::bit_reversal)
+    ; 
+
+    m.def("run_statevector_complex64", py_run_statevector_complex64);
+    m.def("run_pauli_sigma_complex64", py_run_pauli_sigma_complex64);
+    m.def("run_pauli_expectation_value_complex64", py_run_pauli_expectation_value_complex64);
+    m.def("run_pauli_expectation_value_gradient_complex64", py_run_pauli_expectation_value_gradient_complex64);
+
+    // => complex128 <= //
 
     py::class_<vulcan::complex128>(m, "complex128")
     .def(py::init<double, double>())
